cdbb6ed9d090135d996b8989450556ef18082b2f
aishik-pyne <aishikpyne@gmail.com>
2023-09-05 03:45:54 +0800

    Updating theme and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,6 @@ module.exports = {
         'tablet': '640px',
         'desktop': '1024px'
       },
-      fontSize: {
-        sm: '0.8rem',
-        base: '1rem',
-        xl: '1.25rem',
-        '2xl': '1.563rem',
-        '3xl': '1.953rem',
-        '4xl': '2.441rem',
-        '5xl': '3.052rem',
-      },
       fontFamily: {
         signature: ['Radicalis']
       }
